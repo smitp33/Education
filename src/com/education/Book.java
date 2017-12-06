672d9036447f3b50a1798f40b1fc56f6253c59f1
unknown <pjsmith@BTNDG15GC2.ads.aexp.com>
2017-12-06 15:40:13 +0000

    0.0.1

---
+++ src/com/education/Book.java
@@ -0,0 +1,23 @@
+package com.education;
+
+public class Book {
+
+    private String name;
+    private int id;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
